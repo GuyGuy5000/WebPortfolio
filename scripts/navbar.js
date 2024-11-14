529daa538f65a29349b269cbc549ea7d9a65e2e4
GuyGuy5000 <nadavhilu12@gmail.com>
2024-11-14 17:19:36 -0500

    adjusted site map to include more project pages. Fixed nav bar styling

--- scripts/navbar.js
+++ scripts/navbar.js
@@ -2,19 +2,32 @@ navContainer = document.getElementById("navContainer");
 btnOpenNav = document.getElementById("btnOpenNav");
 btnCloseNav = document.getElementById("btnCloseNav");
 
-btnOpenNav.addEventListener("click", ()=>{
-    btnOpenNav.style.display = "none";
-    navContainer.style.display = "flex";
+btnOpenNav.addEventListener("click", () => {
+  toggleNav();
 });
 
-btnCloseNav.addEventListener("click", ()=>{
-    btnOpenNav.style.display = "flex";
-    navContainer.style.display = "none";
+btnCloseNav.addEventListener("click", () => {
+  toggleNav();
 });
 
-addEventListener("resize", (e) =>{
-    if (window.innerWidth >= 440) {
-        btnOpenNav.style.display = "none";
-        navContainer.style.display = "flex";
-    }
-})
+function toggleNav() {
+  btnOpenNav.classList.toggle("hide");
+  btnOpenNav.classList.toggle("show-flex");
+  navContainer.classList.toggle("hide");
+  navContainer.classList.toggle("show-flex");
+}
+
+addEventListener("resize", (e) => {
+  if (window.innerWidth <= 768) {
+    btnOpenNav.classList.remove("hide");
+    btnOpenNav.classList.add("show-flex");
+    navContainer.classList.remove("show-flex");
+    navContainer.classList.add("hide");
+  }
+  else {
+    btnOpenNav.classList.remove("show-flex");
+    btnOpenNav.classList.add("hide");
+    navContainer.classList.remove("hide");
+    navContainer.classList.add("show-flex");
+  }
+});
