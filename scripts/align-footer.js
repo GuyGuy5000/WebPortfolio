d9f90d2ec98f777d44bfb872ffacee8767879bb3
GuyGuy5000 <nadavhilu12@gmail.com>
2024-11-18 17:19:45 -0500

    minor changes across all pages.
    - Added image maximization.
    - Added dynamic footer spacing script.

---
+++ scripts/align-footer.js
@@ -0,0 +1,37 @@
+var footer = document.getElementById("footer");
+const body = document.body;
+const html = document.documentElement;
+
+let maxHeight = Math.max(
+  body.scrollHeight,
+  body.offsetHeight,
+  html.clientHeight,
+  html.scrollHeight,
+  html.offsetHeight
+);
+
+if (window.innerHeight < maxHeight) {
+  footer.style.position = "relative";
+  html.style.marginBottom = "0px";
+} else {
+  footer.style.position = "fixed";
+  html.style.marginBottom = "100px";
+}
+
+addEventListener("resize", (e) => {
+  maxHeight = Math.max(
+    body.scrollHeight,
+    body.offsetHeight,
+    html.clientHeight,
+    html.scrollHeight,
+    html.offsetHeight
+  );
+
+  if (window.innerHeight < maxHeight) {
+    footer.style.position = "relative";
+    html.style.marginBottom = "0px";
+  } else {
+    footer.style.position = "fixed";
+    html.style.marginBottom = "100px";
+  }
+});
